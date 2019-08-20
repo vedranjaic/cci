97b16d0216c1e2a17bfd640613954b6ee5add4d7
Vedran Jaic <vedranjaic@icloud.com>
2019-08-20 11:20:25 +0200

    Wizard, Sidebar, Login and Charts

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -121,8 +121,115 @@ $('#temp-help-02-toggle').click(function(e) {
 
 
 
-
+// Disable grid in Chart.js
 Chart.defaults.scale.gridLines.display = false;
+
+// Extend Chart.js to curve the top bars
+// draws a rectangle with a rounded top
+Chart.helpers.drawRoundedTopRectangle = function(ctx, x, y, width, height, radius) {
+    ctx.beginPath();
+    ctx.moveTo(x + radius, y);
+    // top right corner
+    ctx.lineTo(x + width - radius, y);
+    ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+    // bottom right   corner
+    ctx.lineTo(x + width, y + height);
+    // bottom left corner
+    ctx.lineTo(x, y + height);
+    // top left   
+    ctx.lineTo(x, y + radius);
+    ctx.quadraticCurveTo(x, y, x + radius, y);
+    ctx.closePath();
+  };
+  
+  Chart.elements.RoundedTopRectangle = Chart.elements.Rectangle.extend({
+    draw: function() {
+      var ctx = this._chart.ctx;
+      var vm = this._view;
+      var left, right, top, bottom, signX, signY, borderSkipped;
+      var borderWidth = vm.borderWidth;
+  
+      if (!vm.horizontal) {
+        // bar
+        left = vm.x - vm.width / 2;
+        right = vm.x + vm.width / 2;
+        top = vm.y;
+        bottom = vm.base;
+        signX = 1;
+        signY = bottom > top? 1: -1;
+        borderSkipped = vm.borderSkipped || 'bottom';
+      } else {
+        // horizontal bar
+        left = vm.base;
+        right = vm.x;
+        top = vm.y - vm.height / 2;
+        bottom = vm.y + vm.height / 2;
+        signX = right > left? 1: -1;
+        signY = 1;
+        borderSkipped = vm.borderSkipped || 'left';
+      }
+  
+      // Canvas doesn't allow us to stroke inside the width so we can
+      // adjust the sizes to fit if we're setting a stroke on the line
+      if (borderWidth) {
+        // borderWidth shold be less than bar width and bar height.
+        var barSize = Math.min(Math.abs(left - right), Math.abs(top - bottom));
+        borderWidth = borderWidth > barSize? barSize: borderWidth;
+        var halfStroke = borderWidth / 2;
+        // Adjust borderWidth when bar top position is near vm.base(zero).
+        var borderLeft = left + (borderSkipped !== 'left'? halfStroke * signX: 0);
+        var borderRight = right + (borderSkipped !== 'right'? -halfStroke * signX: 0);
+        var borderTop = top + (borderSkipped !== 'top'? halfStroke * signY: 0);
+        var borderBottom = bottom + (borderSkipped !== 'bottom'? -halfStroke * signY: 0);
+        // not become a vertical line?
+        if (borderLeft !== borderRight) {
+          top = borderTop;
+          bottom = borderBottom;
+        }
+        // not become a horizontal line?
+        if (borderTop !== borderBottom) {
+          left = borderLeft;
+          right = borderRight;
+        }
+      }
+  
+      // calculate the bar width and roundess
+      var barWidth = Math.abs(left - right);
+      var roundness = this._chart.config.options.barRoundness || 0.5;
+      var radius = barWidth * roundness * 0.5;
+  
+      // keep track of the original top of the bar
+      var prevTop = top;
+  
+      // move the top down so there is room to draw the rounded top
+      top = prevTop + radius;
+      var barRadius = top - prevTop;
+  
+      ctx.beginPath();
+      ctx.fillStyle = vm.backgroundColor;
+      ctx.strokeStyle = vm.borderColor;
+      ctx.lineWidth = borderWidth;
+  
+      // draw the rounded top rectangle
+      Chart.helpers.drawRoundedTopRectangle(ctx, left, (top - barRadius + 1), barWidth, bottom - prevTop, barRadius);
+  
+      ctx.fill();
+      if (borderWidth) {
+        ctx.stroke();
+      }
+  
+      // restore the original top value so tooltips and scales still work
+      top = prevTop;
+    },
+  });
+
+  Chart.defaults.roundedBar = Chart.helpers.clone(Chart.defaults.bar);
+
+    Chart.controllers.roundedBar = Chart.controllers.bar.extend({
+    dataElementType: Chart.elements.RoundedTopRectangle
+    });
+
+
 
 // BAR CHART
 var barChartData = {
@@ -258,18 +365,22 @@ var config = {
         ]
     },
     options: {
+        cutoutPercentage: 60,
         responsive: true,
-        // maintainAspectRatio : false,
+        maintainAspectRatio : false,
         legend: {
             position: 'top',
+            display: false,
             labels: {
                 // padding: 20
             }
         },
         layout: {
             padding: {
-                top: 40,
-                bottom: 50
+                top: 20,
+                bottom: 0,
+                left: 40,
+                right: 40
             }
         }
     }
@@ -283,38 +394,84 @@ window.onload = function() {
 
     // Bar chart
     var ctx = document.getElementById('chartCostHistory').getContext('2d');
-    window.myBar = new Chart(ctx, {
+    var chart = new Chart(ctx, {
         type: 'bar',
         data: barChartData,
         options: {
+            barRoundness: 0.5,
             tooltips: {
                 mode: 'index',
-                intersect: false
+                intersect: false,
+            },
+            legend: {
+                display: false
+            },
+            layout: {
+                padding: {
+                    bottom: -9,
+                },
             },
             responsive: true,
             maintainAspectRatio: false,
             scales: {
                 xAxes: [{
                     stacked: true,
-                    categoryPercentage: 0.9,
-                    barPercentage: 0.9,
+                    categoryPercentage: 0.80,
+                    barPercentage: 0.80,
+                    
                     ticks: {
-                        fontColor: "#aaa",
-                        display: false,
+                        fontColor: "#c6d0d5",
+                        // display: false,
                     }
                 }],
                 yAxes: [{
                     stacked: true,
-                    display: false,
+                    // display: false,
                     ticks: {
-                        display: false
+                        fontColor: "#c6d0d5",
+                        // display: false
                     }
                 }]
             }
         }
     });
 
+    // Put legend in separate div
+    var myLegendContainer = document.getElementById("legend");
+    // generate HTML legend
+    myLegendContainer.innerHTML = chart.generateLegend();
+
     // Pie chart
     var ctxPie = document.getElementById('chartCostThisMonth').getContext('2d');
     window.myPie = new Chart(ctxPie, config);
 };
+
+
+
+
+// let num = 50;
+
+// const logNum = () => {
+//     num = 100;
+//     console.log(num);
+// }
+
+// logNum();
+// console.log(num);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
